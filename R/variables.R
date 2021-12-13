8b370c04a712898149a9a4d1ab12c4edc765f8ee
seedpcseed <seedpcseed@gmail.com>
2021-12-13 11:28:15 -0600

    updated classify.def

--- R/variables.R
+++ R/variables.R
@@ -5,7 +5,7 @@
 #' Ann and Robert H. Lurie Children's Hospital
 #' Northwestern University
 #' MIT license
-#' 
+#'
 #' Define variables
 
 library(tidyverse)
@@ -27,7 +27,7 @@ printGlue("#----------------------------------------------------------------")
 projectdir = getProjDir()
 basedir = "/mnt" # basedir = dirname(projectdir)
 projectname = basename(projectdir)
-  
+
 current.step = commands[[1]]
 in_ext <<- current.step$in_ext
 split_ext <<- current.step$split_ext
@@ -46,11 +46,10 @@ samples <<- sapply(list.files(dirNormalize(current.step$from), pattern = current
 # SLURM variables ---------------------------------------------------------
 
 partition = "genomics"; account = "b1042"
-#partition = "buyin"; account = "b1051"
 mem = "96G"
 time = "08:00:00" #this may change based on steps
 N = 1
-ntasks_per_node = threads = 28 #only allow 28 for b1051/buyin; can have up to 52 for b1042/genomics
+ntasks_per_node = threads = 28
 
 # containers  --------------------------------------------------------------
 
@@ -61,37 +60,6 @@ assembly.by.refseq = stringr::str_glue("{singularity} containers/assembly-by-ref
 metaerg = stringr::str_glue("{singularity}containers/assembly-by-refseq.sif ")
 coverm = stringr::str_glue("{singularity} containers/assembly-by-refseq.sif ")
 semibin = stringr::str_glue("{singularity} run -B {basedir} containers/assembly-by-refseq.sif ")
-
-fastp = stringr::str_glue("{singularity} containers/fastp.sif ")
-bowtie = stringr::str_glue("{singularity} containers/bowtie2.sif ")
-samtools = stringr::str_glue("{singularity} containers/samtools.sif ")
-kaiju = stringr::str_glue("{singularity} containers/kaiju.sif ")
-skesa = stringr::str_glue("{singularity} containers/skesa.sif ")
-metaprokka = stringr::str_glue("{singularity} containers/metaprokka.sif ")
-abricate = stringr::str_glue("{singularity} containers/abricate.sif ")
-seqtk = stringr::str_glue("{singularity} containers/seqtk.sif ")
-metahipmer = stringr::str_glue("{singularity} containers/metahipmer.sif ")
-bbtools = stringr::str_glue("{singularity} containers/bbtools.sif ")
-eggnogmapper = stringr::str_glue("{singularity} containers/eggnog-mapper.sif ")
-squeezemeta = stringr::str_glue("{singularity} containers/squeezemeta.sif ")
-pigz = stringr::str_glue("{singularity} containers/pigz.sif ")
-idseq.dedup = stringr::str_glue("{singularity} containers/idseq-dedup.sif ")
-decontaminate = stringr::str_glue("{singularity} containers/decontaminate.sif ")
-minimap2 = stringr::str_glue("{singularity} containers/minimap2_v2.15dfsg-1-deb_cv1.sif")
-seqstats = stringr::str_glue("{singularity} containers/seqstats.sif ")
-flash = stringr::str_glue("{singularity} containers/pangenome.sif ")
-mash = stringr::str_glue("{singularity} containers/pangenome.sif ")
-sambamba = stringr::str_glue("{singularity} containers/pangenome.sif ")
-cdhit = stringr::str_glue("{singularity} containers/squeezemeta.sif ")
-megahit = stringr::str_glue("{singularity} containers/pangenome.sif ")
-orna = stringr::str_glue("{singularity} containers/orna.sif ")
-quickmerge = stringr::str_glue("{singularity} containers/pangenome.sif ")
-prinseq = stringr::str_glue("{singularity} containers/squeezemeta.sif ")
-humann = stringr::str_glue("{singularity} containers/humann3.sif ")
-seqkit = stringr::str_glue("{singularity} containers/seqkit.sif")
-MAC = stringr::str_glue("{singularity} containers/MAC.sif")
-flye = stringr::str_glue("{singularity} containers/flye.sif ")
-abricate = stringr::str_glue("{singularity} containers/abricate.sif ")
 
 # MODULE-SPECIFIC VARIABLES -----------------------------------------------
 
@@ -109,12 +77,9 @@ variables.idseq.dedup = stringr::str_glue("-l 70")
 variables.metaprokka.merged = stringr::str_glue(" --cpus {threads} --mincontiglen 500 --force ")
 variables.decontaminate = c("fna$", "databases/contaminants/human/", "databases/contaminants/mouse/",
                             "databases/contaminants/phiX/")
-variables.squeezemeta.merged = stringr::str_glue(" -m merged -t {threads} --nopfam --extdb databases/squeezemeta ")
-variables.squeezemeta.sequential = stringr::str_glue(" -m sequential -t {threads} --nopfam --extdb databases/squeezemeta ")
 variables.pigz = stringr::str_glue(" -p {threads} ")
 variables.megahit = stringr::str_glue(" -t {threads}  --memory 0.8 ")
 variables.flash = stringr::str_glue(" -t 4 -M 150 ")
-variables.orna = stringr::str_glue("-kmer 27 -base 1.7 -nb-cores {threads}")
 variables.humann = stringr::str_glue("--threads {threads} --remove-temp-output --nucleotide-database databases/humann/chocophlan --protein-database databases/humann/uniref --metaphlan-options '--bowtie2db /opt/miniconda-4.6.14/envs/biobakery/lib/python3.7/site-packages/metaphlan/metaphlan_databases' ")
 variables.flye = stringr::str_glue("--threads {threads} --iterations 0 --meta --subassemblies ")
 variables.abricate = stringr::str_glue("--threads {threads} --db ncbi --nopath ")
@@ -127,45 +92,14 @@ methods = list(
   mmseqs= "Taxonomy was assigned by the 2bLCA method using the tool mmseq2 and the reference database {references$mmseq}.",
   kaiju = "Taxonomy was assigned using the tool kaiju and the reference database {references$kaiju}.",
   megahit = "Pre-processed metagenomic sequences were de novo assembled using the tool MEGAHIT {references$megahit}.",
-  kneaddata = "",
-  skesa = " Pre-processed metagenomic sequences were de novo assembled using the tool SKESA {references$skesa}. ",
-  abricate = "", 
-  seqtk = "",
-  metahipmer = "",
   bbtools = "",
-  squeezemeta = "",
   pigz = ""
 )
 
 # DATABASE REFERENCES -----------------------------------------------------
 
-database.references = list()
-
-# WHERE TO PULL CONTAINERS ------------------------------------------------
-
-containers.pull = list(
-  samtools =list("biocontainers/samtools:v1.9-4-deb_cv1" , "samtools.sif"),
-  fastp = list( "biocontainers/fastp:v0.20.1_cv1", "fastp.sif" ),
-  bowtie2 =  list("biocontainers/bowtie2:v2.4.1_cv1", "bowtie2.sif" ),
-  kaiju = list("nanozoo/kaiju:1.7.2--fa366a0", "kaiju.sif"),
-  skesa = list( "staphb/skesa:2.4.0", "skesa.sif"),
-  abricate = list("staphb/abricate:1.0.0", "abricate.sif"),
-  seqk = list("biocontainers/seqtk:v1.3-1-deb_cv1", "seqtk.sif"),
-  bbtools = list("bryce911/bbtools:38.90", "bbtools.sif"),
-  pigz = list("rtibiocloud/pigz:v2.4_b243f9", "pigz.sif")
-)
-
-containers.build = list(
-  metaprokka = list("definitions/metaprokka.def", "metaprokka.sif"),
-  r_base = list("definitions/r-base.def", "r-base.sif"),
-  eggnog_mapper = list("definitions/eggnog-mapper.def", "eggnog-mapper.sif"),
-  squeezemeta = list("definitions/squeezemeta.v2.def", "squeezemeta.sif"),
-  seqstats = list("definitions/seqstats.def", "seqstats.sif")
-)
-
-# decorations
+# DECORATIONS
 indent1 = "#---- "
 indent2 = "#-------- "
 indent3 = "#------------ "
 indent4 = "#---------------- "
-
